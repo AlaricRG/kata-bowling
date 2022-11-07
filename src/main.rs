5e32350048c8f99c2297bf1ed7cbcabb41d4d16e
AlaricRG <alaric.rougnon.glasson@gmail.com>
2022-11-07 16:45:31 +0100

    ft: tested spare

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
-fn bowling_score(score_sequence:Vec<u8>) -> u16 {
-   score_sequence.into_iter().reduce(|a, b| a + b).unwrap().into()
+fn get_bowling_score(score_sequence:Vec<u8>) -> u16 {
+    let spare_indices = get_indices_of_scores_to_double_because_of_spare(&score_sequence);
+    score_sequence
+        .into_iter()
+        .enumerate()
+        .map(|(index, score)| {
+            if spare_indices.contains(&index) {
+                score * 2
+            } else {
+                score
+            }
+        })
+        .reduce(|a, b| a + b).unwrap().into()
+}
+
+fn get_indices_of_scores_to_double_because_of_spare(score_sequence: &Vec<u8>) -> Vec<usize>{
+    let mut indices_to_return = Vec::new();
+    let spare_score = 10;
+    for (i, knocked_pins) in score_sequence.chunks(2).enumerate() {
+        if knocked_pins[0] + knocked_pins[1] == spare_score {
+            indices_to_return.push(i*2+2)
+        }
+    }
+    indices_to_return
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_basics {
     use super::*;
 
     #[test]
     fn should_return_0_when_sequence_is_full_of_0() {
         let sequence = vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        assert_eq!(bowling_score(sequence), 0);
+        assert_eq!(get_bowling_score(sequence), 0);
+    }
+
+    #[test]
+    fn should_return_1_when_sequence_has_1_then_full_0() {
+        let sequence = vec![1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        assert_eq!(get_bowling_score(sequence), 1);
+    }
+
+    mod test_spare {
+        use super::*;
+
+        #[test]
+        fn should_return_16_when_sequence_has_2_8_3_then_full_0() {
+            let sequence = vec![2,8,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+            assert_eq!(get_bowling_score(sequence), 16);
+        }
     }
 }
+
+fn main() {}
