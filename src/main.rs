72dd5d30e22c12622f7613a5acaf0ca1243de2fb
AlaricRG <alaric.rougnon.glasson@gmail.com>
2022-11-07 14:46:53 +0100

    Ft: should_return_0_when_sequence_is_full_of_0 and should_return_1_when_sequence_has_1_then_full_0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn bowling_score(score_sequence:Vec<u8>) -> u16 {
+   score_sequence.into_iter().reduce(|a, b| a + b).unwrap().into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_0_when_sequence_is_full_of_0() {
+        let sequence = vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        assert_eq!(bowling_score(sequence), 0);
+    }
 }
